1ee639df0cb5ce098828daaee83e85ce88550a23
Izhar Ahmad <izhar.ahmad@fisglobal.com>
2018-03-23 12:26:23 +0530

    test commit

--- src/com/mypackage/Testimmutablestring.java
+++ src/com/mypackage/Testimmutablestring.java
@@ -4,6 +4,5 @@ class Testimmutablestring{
    String s="Sachin";  
    String z = s.concat(" Tendulkar");//concat() method appends the string at the end  
    System.out.println(s);//will print Sachin because strings are immutable objects 
-   System.out.println(z);
  }  
 }  
